76f27aa18505a319bf71a6a4172e00f8a41a0a85
Brian <klanestrotalisman@gmail.com>
2010-01-20 01:57:44 -0800

    got alex vortaro working

--- preniVortoj.php
+++ preniVortoj.php
@@ -2,29 +2,48 @@
 <body>
 
 <?php
-
 // Created by Talisman 01/2010 ★✩ 
-//require 'HTTP/Request.php';
+
 $vorto = $_GET['vorto']; // Get the Word from Outer Space and Search for it!
 
 if (isset($vorto))
 	{
 	echo $vorto;
-	return;
 	} else {
-//		$vorto = "";
-		$Help = "No Vorto add ?vorto=TheWordYouWant to the end of this website";
+		$Help = "No Vorto -> add ?vorto=TheWordYouWant to the end of this website";
 		echo $Help;
 	}
 	
 
+//$v =  fopen("http://vortaro.us.to/ajax/epo/eng/' + $vorto + '/?callback=?', "r");
 
 
 // Now Lets Search Alex's Vortareo
 //	ex. http://vortaro.us.to/ajax/epo/eng/petas/?callback=?
 // Future Feature inproved language functinality
 
-//$v1 = fopen('http://vortaro.us.to/ajax/epo/eng/' + $vorto + '/?callback=?',r) or die($php_errormsg);
+$url1 = "http://vortaro.us.to/ajax/epo/eng/"; 
+$url2 = "/?callback=?";
+$finalurl= $url1 . $vorto . $url2;
+echo $finalurl;
+echo "<br>";
+
+$content =file_get_contents($finalurl) ;
+echo $content;
+
+/*
+
+$v1 = fopen($finalurl ,"r");
+echo $v1;
+
+
+$frv1 = fread($v1,filesize($v1));
+echo $frv1 ;
+
+//$Help2 = "did echo v1 does internet work?";
+
+//echo $Help2;
+*/
 
 ?>
 

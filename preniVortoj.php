5bd06071afa2038d4559af80a2a58133bd05cc0a
Brian <klanestrotalisman@gmail.com>
2010-01-19 23:43:38 -0800

    fikulino

--- preniVortoj.php
+++ preniVortoj.php
@@ -4,16 +4,27 @@
 <?php
 
 // Created by Talisman 01/2010 ★✩ 
-
-
-
+//require 'HTTP/Request.php';
 $vorto = $_GET['vorto']; // Get the Word from Outer Space and Search for it!
-echo $vorto
+
+if (isset($vorto))
+	{
+	echo $vorto;
+	return;
+	} else {
+//		$vorto = "";
+		$Help = "No Vorto add ?vorto=TheWordYouWant to the end of this website";
+		echo $Help;
+	}
+	
 
 
-// Now Lets Search Alex's Vortareo // I Don't know how he made it except its easy for ajax.
 
+// Now Lets Search Alex's Vortareo
+//	ex. http://vortaro.us.to/ajax/epo/eng/petas/?callback=?
+// Future Feature inproved language functinality
 
+//$v1 = fopen('http://vortaro.us.to/ajax/epo/eng/' + $vorto + '/?callback=?',r) or die($php_errormsg);
 
 ?>
 

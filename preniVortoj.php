b56ff101b08a0667d282303aa4e7422eb33cb9ae
Brian <klanestrotalisman@gmail.com>
2010-01-19 22:46:08 -0800

    ?vorto=

---
+++ preniVortoj.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+
+<?php
+
+// Created by Talisman 01/2010 ★✩ 
+
+
+
+$vorto = $_GET['vorto']; // Get the Word from Outer Space and Search for it!
+echo $vorto
+
+
+// Now Lets Search Alex's Vortareo // I Don't know how he made it except its easy for ajax.
+
+
+
+?>
+
+</body>
+</html>
